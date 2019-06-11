e3c6e7e44800118088f88bdd33ddddeac12ebf3e
kek <kek@awa.com>
2019-06-11 12:45:29 +0700

    score script

---
+++ Assets/script/score.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class score : MonoBehaviour
+{
+    public float level = 1f;
+    public float scoreCount = 0f;
+    public float levelCalculation;
+    [SerializeField] Text scoreCounter;
+    
+    void Start()
+    {
+        levelCalculation = level / 10;
+    }
+    
+    void Update()
+    {
+        scoreCount += levelCalculation * Time.deltaTime; //Increment the score
+        scoreCounter.text = scoreCount.ToString();        
+    }
+}
